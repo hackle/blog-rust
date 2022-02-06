24452c5c7e09e8acdcc67833ab8b1c829f11b7de
Hacke Wayne <hacklew@hotmail.com>
2022-02-06 15:33:03 +1300

    non i ntereatvei

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ set -euxo pipefail
 
 docker build -t lambda_builder -f lambdaweb.Dockerfile .
 
-docker run -it --rm -v ~/.cargo/registry:/root/.cargo/registry:z -v $(pwd):/build:z lambda_builder
+docker run -t --rm -v ~/.cargo/registry:/root/.cargo/registry:z -v $(pwd):/build:z lambda_builder
 
 serverless deploy --stage "prod"
