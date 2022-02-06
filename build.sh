ee8e398431d44a8c545b13e5929597f60b22469a
Hacke Wayne <hacklew@hotmail.com>
2022-02-06 15:49:30 +1300

    with secrets

--- build.sh
+++ build.sh
@@ -9,5 +9,3 @@ set -euxo pipefail
 docker build -t lambda_builder -f lambdaweb.Dockerfile .
 
 docker run -t --rm -v ~/.cargo/registry:/root/.cargo/registry:z -v $(pwd):/build:z lambda_builder
-
-serverless deploy --stage "prod"
