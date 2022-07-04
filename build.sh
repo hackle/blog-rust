7be211940eb7c15a507758c22c888c38cb22ff5c
Hacke Wayne <hacklew@hotmail.com>
2022-07-04 15:21:28 +1200

    run tests

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 set -euxo pipefail
 
-#docker build . -t 318142272849.dkr.ecr.ap-southeast-2.amazonaws.com/hacks-blog-rust:latest
-#
-#docker push 318142272849.dkr.ecr.ap-southeast-2.amazonaws.com/hacks-blog-rust:latest
-
 docker build -t lambda_builder -f lambdaweb.Dockerfile .
 
 docker run -t --rm -v ~/.cargo/registry:/root/.cargo/registry:z -v $(pwd):/build:z lambda_builder
